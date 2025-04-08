6ebd2520825a81f2392942dc6bc212b3efe2865e
Arman Akhmetov <eon704@gmail.com>
2025-04-07 21:40:50 -0400

    UI

--- Assets/Game/Scripts/Platform/PlatformArea.cs
+++ Assets/Game/Scripts/Platform/PlatformArea.cs
@@ -1,3 +1,4 @@
+using System;
 using Events;
 using UnityEngine;
 
@@ -5,6 +6,23 @@ namespace Platform
 {
   public class PlatformArea : MonoBehaviour
   {
+    private bool isWorking = true;
+    
+    private void Start()
+    {
+      G.EventManager.Register<OnPlayerDeath>(OnGameOver);
+    }
+
+    private void OnDestroy()
+    {
+      G.EventManager.Unregister<OnPlayerDeath>(OnGameOver);
+    }
+    
+    private void OnGameOver(OnPlayerDeath e)
+    {
+      this.gameObject.SetActive(false);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
       if (collision.CompareTag("Player"))
